7e4e332ca078fa67e7c4aa9fda0f867ebbd938f0
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-04-19 11:56:47 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter,
+  },
+  data() {
+    return {
+
+    }
+  },
+}
+</script>
+
+<template>
+  <h1 class=" text">Boolflix</h1>
+
+  <template>
+    <SiteHeader></SiteHeader>
+    <SiteMain></SiteMain>
+    <SiteFooter></SiteFooter>
+  </template>
+</template>
+
+<style lang="scss">
+@use '../assets/scss/partials/variables' as *;
+</style>
