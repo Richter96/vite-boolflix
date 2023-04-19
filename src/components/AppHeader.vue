88138e21fda70b250a15fa137b4b0b9f6e7fa5a7
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-04-19 22:01:36 +0200

    add generateTv in appHeder

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -18,7 +18,7 @@ export default {
 <template>
     <div>
         <h1 class=" text-uppercase">Boolflix</h1>
-        <input type="text" placeholder="Cerca un film" v-model="store.filmSearch">
-        <button @click="store.generateFilms()">Search</button>
+        <input type="text" placeholder="Cerca un film" v-model="store.userSearch">
+        <button @click="store.generateFilms() & store.generateTv()">Search</button>
     </div>
 </template>
