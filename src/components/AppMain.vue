bd488056b9357f1652bdaea4b3478c7fcb47cc9a
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-04-19 16:58:45 +0200

    complate milestone 1

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -18,10 +18,10 @@ export default {
     <div>
         <div class="Card_film d-flex row row-cols-5">
             <div class="col border border-2" v-if="store.filmsFound != ''" v-for="film in store.filmsFound">
-                <p>titolo: {{ film.title }}</p>
-                <p>titolo originale: {{ film.original_title }}</p>
-                <p>lingua: {{ film.original_language }}</p>
-                <p>voto: {{ film.vote_average }}</p>
+                <p>Titolo: {{ film.title }}</p>
+                <p>Titolo originale: {{ film.original_title }}</p>
+                <p>Lingua: {{ film.original_language }}</p>
+                <p>Voto: {{ film.vote_average }}</p>
             </div>
         </div>
     </div>
