ce8a93137c29528e236462ccb8dced31a7531b8d
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-04-20 10:51:35 +0200

    tray url image

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -18,6 +18,7 @@ export default {
 <template>
     <div>
         <h1 class=" text-uppercase">Boolflix</h1>
+        <img src="https://image.tmdb.org/t/p/w342/wwemzKWzjKYJFfCeiB57q3r4Bcm.png" alt="">
         <input type="text" placeholder="Cerca un film" v-model="store.userSearch">
         <button @click="store.generateFilms() & store.generateTv()">Search</button>
     </div>
